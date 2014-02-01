74cccb6a3ec1e29b7596822203be752152f0831b
Nazma Panjwani <nazpanj@u-vlos.csc.uvic.ca>
2014-02-01 09:09:27 -0800

    added test.py

--- py/test.py
+++ py/test.py
@@ -1,3 +1,15 @@
-#!/usr/bin/python2.6
+#!/usr/bin/env python
+import html
 import cgi
-cgi.test()
+import cgitb; cgitb.enable()     # for troubleshooting
+
+print("Content-Type: text/html") # HTTP header to say HTML is following
+print()                          # blank line, end of headers
+
+form = cgi.FieldStorage()
+item_name  = html.escape(form["item_name"].value);
+#item_price   = html.escape(form["item_price"].value);
+
+print(item_name)
+
+
